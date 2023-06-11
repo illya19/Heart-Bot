96a5ab0a493de55fc0d49d16667687eaae5db3c2
illya19 <131199964+illya19@users.noreply.github.com>
2023-06-11 12:09:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from replit_keep_alive import WaitressStart
+
+WaitressStart() 
 
 logging.basicConfig(level=logging.INFO)
 
